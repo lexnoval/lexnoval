ca8d65dee50408f9933d0a37c329a26dd2d376f0
lexnoval <cihangir1991@icloud.com>
2025-08-24 00:38:59 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // gerekirse başka ayarları buraya ekleyebilirsin
+  // başka ayarlar gerekiyorsa buraya ekleyebilirsiniz
 };
 
 export default nextConfig;
-
